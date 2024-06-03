ceef83a378863814130f2efec378213d4114a71e
Peter Zhigalov <peter.zhigalov@gmail.com>
2024-06-04 03:09:51 +0700

    Add support for Audacious 4.4-beta1

--- build_msys2.sh
+++ build_msys2.sh
@@ -5,8 +5,15 @@ SOURCE_DIR="$(cd "$(dirname "${0}")" && pwd)"
 BUILD_DIR="${PWD}/build-${MSYSTEM,,?}"
 
 MSYSTEM_PKG_PREFIX="mingw-w64"
+QT_MAJOR_VERSION=""
 if [ "${MSYSTEM}" == "MINGW32" ] ; then
+    # Audacious <= 4.3.1
     MSYSTEM_PKG_PREFIX="${MSYSTEM_PKG_PREFIX}-i686"
+    QT_MAJOR_VERSION="5"
+elif [ "${MSYSTEM}" == "MINGW64" ] ; then
+    # Audacious >= 4.4-beta1
+    MSYSTEM_PKG_PREFIX="${MSYSTEM_PKG_PREFIX}-x86_64"
+    QT_MAJOR_VERSION="6"
 else
     echo "Unknown or broken MSYSTEM: ${MSYSTEM}"
     exit 1
@@ -14,7 +21,7 @@ fi
 pacman -S --needed --noconfirm \
     base-devel \
     ${MSYSTEM_PKG_PREFIX}-toolchain \
-    ${MSYSTEM_PKG_PREFIX}-qt5-base \
+    ${MSYSTEM_PKG_PREFIX}-qt${QT_MAJOR_VERSION}-base \
     ${MSYSTEM_PKG_PREFIX}-cmake
 
 rm -rf "${BUILD_DIR}"
